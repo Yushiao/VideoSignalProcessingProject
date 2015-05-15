1220ca78e6420555dce9a111691e0e505d711b31
Yushiao <asdf168gh@gmail.com>
2015-05-15 14:55:19 +0800

    Update

--- source/JPEGEncoder.cpp
+++ source/JPEGEncoder.cpp
@@ -24,7 +24,9 @@ private:
     int quality;
     unsigned char* image;
 
-    int* block; /// 8x8 block
+    int blockSize;
+    int** block; /// every 8x8 block
+    int blockTotal;
 
 };
 
@@ -55,13 +57,32 @@ void JPEGEncoder::encode(const char* filename, int w, int h, int q)
     fclose(file);
 
     /// partition 8x8 block
-    block = new int[64];
+    partition();
 
 }
 
 void JPEGEncoder::partition()
 {
+    blockSize = 8;
+    int blockWidthLuma, blockheightLuma;
+    int blockWidthChroma, blockheightChroma;
+    blockWidthLuma = (width+blockSize-1)/blockSize;
+    blockheightLuma = (height/2+blockSize-1)/blockSize;
+    blockWidthChroma = (width+blockSize-1)/blockSize;
+    blockheightChroma = (height/2+blockSize-1)/blockSize;
+    blockTotal = blockWidthLuma*blockheightLuma+2*blockWidthChroma*blockheightChroma;
 
+    block = new int*[blockTotal];
+    for(int i=0; i<blockTotal; i++){
+        block[i] = new int[blockSize*blockSize];
+    }
+    for(int b=0; b<blockTotal; b++){
+        for(int i=0; i<blockSize; i++){
+            for(int j=0; j<blockSize; j++){
+                block[b][i*blockSize+j] = 0;
+            }
+        }
+    }
 }
 
 void JPEGEncoder::transform()
