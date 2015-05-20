3043d02afc9f414a395e03021c2889d2c24623b7
Yushiao <asdf168gh@gmail.com>
2015-05-20 22:20:27 +0800

    Update entropy

--- source/JPEGEncoder.cpp
+++ source/JPEGEncoder.cpp
@@ -57,7 +57,7 @@ JPEGEncoder::~JPEGEncoder()
 
 void JPEGEncoder::encode(const char* filename, int w, int h, int q)
 {
-    //#undef DEBUG
+    #undef DEBUG
     #ifndef DEBUG
     /// set parameter
     width = w;
@@ -268,16 +268,20 @@ void JPEGEncoder::entropy()
     static const int lumaDCCodeWordTable[] = {0, 2, 3, 4, 5, 6, 14, 30, 62, 126, 254, 510};
     static const int chromaDCCodeLengthTable[] = {2, 2, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
     static const int chromaDCCodeWordTable[] = {0, 1, 2, 6, 14, 30, 62, 126, 254, 510, 1022, 2046};
+    static const int lumaACCodeLengthTable[] = {4, 2, 2, 3, 4, 5, 7, 8, 10, 16, 16, 0, 4, 5, 7, 9, 11, 16, 16, 16, 16, 16, 0, 5, 8, 10, 12, 16, 16, 16, 16, 16, 16, 0, 6, 9, 12, 16, 16, 16, 16, 16, 16, 16, 0, 6, 10, 16, 16, 16, 16, 16, 16, 16, 16, 0, 7, 11, 16, 16, 16, 16, 16, 16, 16, 16, 0, 7, 12, 16, 16, 16, 16, 16, 16, 16, 16, 0, 8, 12, 16, 16, 16, 16, 16, 16, 16, 16, 0, 9, 15, 16, 16, 16, 16, 16, 16, 16, 16, 0, 9, 16, 16, 16, 16, 16, 16, 16, 16, 16, 0, 9, 16, 16, 16, 16, 16, 16, 16, 16, 16, 0, 10, 16, 16, 16, 16, 16, 16, 16, 16, 16, 0, 10, 16, 16, 16, 16, 16, 16, 16, 16, 16, 0, 11, 16, 16, 16, 16, 16, 16, 16, 16, 16, 0, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 11, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16};
+    static const int lumaACCodeWordTable[] = {10, 0, 1, 4, 11, 26, 120, 248, 1014, 65410, 65411, 0, 12, 27, 121, 502, 2038, 65412, 65413, 65414, 65415, 65416, 0, 28, 249, 1015, 4084, 65417, 65418, 65419, 65420, 65421, 65422, 0, 58, 503, 4085, 65423, 65424, 65425, 65426, 65427, 65428, 65429, 0, 59, 1016, 65430, 65431, 65432, 65433, 65434, 65435, 65436, 65437, 0, 122, 2039, 65438, 65439, 65440, 65441, 65442, 65443, 65444, 65445, 0, 123, 4086, 65446, 65447, 65448, 65449, 65450, 65451, 65452, 65453, 0, 250, 4087, 65454, 65455, 65456, 65457, 65458, 65459, 65460, 65461, 0, 504, 32704, 65462, 65463, 65464, 65465, 65466, 65467, 65468, 65469, 0, 505, 65470, 65471, 65472, 65473, 65474, 65475, 65476, 65477, 65478, 0, 506, 65479, 65480, 65481, 65482, 65483, 65484, 65485, 65486, 65487, 0, 1017, 65488, 65489, 65490, 65491, 65492, 65493, 65494, 65495, 65496, 0, 1018, 65497, 65498, 65499, 65500, 65501, 65502, 65503, 65504, 65505, 0, 2040, 65506, 65507, 65508, 65509, 65510, 65511, 65512, 65513, 65514, 0, 65515, 65516, 65517, 65518, 65519, 65520, 65521, 65522, 65523, 65524, 2041, 65525, 65526, 65527, 65528, 65529, 65530, 65531, 65532, 65533, 65534};
+    static const int chromaACCodeLengthTable[] = {2, 2, 3, 4, 5, 5, 6, 7, 9, 10, 12, 0, 4, 6, 8, 9, 11, 12, 16, 16, 16, 16, 0, 5, 8, 10, 12, 15, 16, 16, 16, 16, 16, 0, 5, 8, 10, 12, 16, 16, 16, 16, 16, 16, 0, 6, 9, 16, 16, 16, 16, 16, 16, 16, 16, 0, 6, 10, 16, 16, 16, 16, 16, 16, 16, 16, 0, 7, 11, 16, 16, 16, 16, 16, 16, 16, 16, 0, 7, 11, 16, 16, 16, 16, 16, 16, 16, 16, 0, 8, 16, 16, 16, 16, 16, 16, 16, 16, 16, 0, 9, 16, 16, 16, 16, 16, 16, 16, 16, 16, 0, 9, 16, 16, 16, 16, 16, 16, 16, 16, 16, 0, 9, 16, 16, 16, 16, 16, 16, 16, 16, 16, 0, 9, 16, 16, 16, 16, 16, 16, 16, 16, 16, 0, 11, 16, 16, 16, 16, 16, 16, 16, 16, 16, 0, 14, 16, 16, 16, 16, 16, 16, 16, 16, 16, 10, 15, 16, 16, 16, 16, 16, 16, 16, 16, 16};
+    static const int chromaACCodeWordTable[] = {0, 1, 4, 10, 24, 25, 56, 120, 500, 1014, 4084, 0, 11, 57, 246, 501, 2038, 4085, 65416, 65417, 65418, 65419, 0, 26, 247, 1015, 4086, 32706, 65420, 65421, 65422, 65423, 65424, 0, 27, 248, 1016, 4087, 65425, 65426, 65427, 65428, 65429, 65430, 0, 58, 502, 65431, 65432, 65433, 65434, 65435, 65436, 65437, 65438, 0, 59, 1017, 65439, 65440, 65441, 65442, 65443, 65444, 65445, 65446, 0, 121, 2039, 65447, 65448, 65449, 65450, 65451, 65452, 65453, 65454, 0, 122, 2040, 65455, 65456, 65457, 65458, 65459, 65460, 65461, 65462, 0, 249, 65463, 65464, 65465, 65466, 65467, 65468, 65469, 65470, 65471, 0, 503, 65472, 65473, 65474, 65475, 65476, 65477, 65478, 65479, 65480, 0, 504, 65481, 65482, 65483, 65484, 65485, 65486, 65487, 65488, 65489, 0, 505, 65490, 65491, 65492, 65493, 65494, 65495, 65496, 65497, 65498, 0, 506, 65499, 65500, 65501, 65502, 65503, 65504, 65505, 65506, 65507, 0, 2041, 65508, 65509, 65510, 65511, 65512, 65513, 65514, 65515, 65516, 0, 16352, 65517, 65518, 65519, 65520, 65521, 65522, 65523, 65524, 65525, 1018, 32707, 65526, 65527, 65528, 65529, 65530, 65531, 65532, 65533, 65534};
 
     int blockElement = blockSize * blockSize;
-    int* run = new int[blockElement];
-    int* number = new int[blockElement];
+    int* run = new int[blockElement+1];
+    int* number = new int[blockElement+1];
     int* runIter;
     int* numberIter;
     int blockIndex;
     int blockEnd;
     int runCount;
-    for(int b=0; b<1; b++){ // FIXME for test
+    for(int b=0; b<blockTotal; b++){ // FIXME for test
         runIter = run;
         numberIter = number;
         blockIndex = b*blockElement;
@@ -326,7 +330,7 @@ void JPEGEncoder::entropy()
         int category = *numberIter;
         category = (category<0) ? -category : category;
         category = numberOfBits(category);
-        if(category>11) cout << "ERROR DC too large" << endl;
+        if(category>11) cout << "ERROR DC value too large" << endl;
         if(b < blockLumaTotal){ /// luma
             pushBits(lumaDCCodeLengthTable[category], lumaDCCodeWordTable[category]);
         }else{ /// chroma
@@ -338,12 +342,28 @@ void JPEGEncoder::entropy()
         /// for AC value
         while(*runIter!=0 || *numberIter!=0){
             /// TODO encode AC to bitstream
+            category = *numberIter;
+            category = (category<0) ? -category : category;
+            category = numberOfBits(category);
+            if(category>=11) cout << "ERROR AC value too large" << endl;
+            if(category==0 && (*runIter!=15)) cout << "ERROR run value" << endl;
 
+            if(b < blockLumaTotal){ /// luma
+                pushBits(lumaACCodeLengthTable[(*runIter)*11 + category], lumaACCodeWordTable[(*runIter)*11 + category]);
+            }else{ /// chroma
+                pushBits(chromaACCodeLengthTable[(*runIter)*11 + category], chromaACCodeWordTable[(*runIter)*11 + category]);
+            }
+            pushBits(category, *numberIter);
             runIter++;
             numberIter++;
         }
+        /// EOB
+        if(b < blockLumaTotal){ /// luma
+            pushBits(lumaACCodeLengthTable[0], lumaACCodeWordTable[0]);
+        }else{ /// chroma
+            pushBits(chromaACCodeLengthTable[0], chromaACCodeWordTable[0]);
+        }
     }
-    cout << endl;
 }
 
 int JPEGEncoder::divCeil(int dividend, int divisor)
@@ -374,6 +394,7 @@ int JPEGEncoder::numberOfBits(int number)
 
 void JPEGEncoder::pushBits(int length, int number)
 {
+    if(length<0) cout << "ERROR! length is negative" << endl;
     int bit = 1 << length;
     bool b;
     if(number < 0) number = ~(-number);
@@ -382,10 +403,6 @@ void JPEGEncoder::pushBits(int length, int number)
         b = number&bit;
         bitstream.push_back(b);
     }
-    for(int i=0; i<bitstream.size(); i++){
-        cout << bitstream[i];
-    }
-    cout << endl;
 }
 
 int main(int argc, char* argv[])
